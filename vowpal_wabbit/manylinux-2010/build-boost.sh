73fae3cc923ec86b44d6a12a5b9018899f9bb5b0
Jack Gerrits <jackgerrits@users.noreply.github.com>
2021-11-30 17:18:09 -0500

    build: add support for python 3.10 (#45)
    * build: add support for python 3.10
    
    * dont check cert
    
    * pickup fix from 1.76

--- vowpal_wabbit/manylinux-2010/build-boost.sh
+++ vowpal_wabbit/manylinux-2010/build-boost.sh
@@ -2,10 +2,10 @@
 set -ex
 
 BOOST_MAJOR=1
-BOOST_MINOR=70
+BOOST_MINOR=76
 BOOST_PATCH=0
 
-wget -O /tmp/boost.tar.gz "https://sourceforge.net/projects/boost/files/boost/${BOOST_MAJOR}.${BOOST_MINOR}.${BOOST_PATCH}/boost_${BOOST_MAJOR}_${BOOST_MINOR}_${BOOST_PATCH}.tar.gz/download"
+wget --no-check-certificate -O /tmp/boost.tar.gz "https://sourceforge.net/projects/boost/files/boost/${BOOST_MAJOR}.${BOOST_MINOR}.${BOOST_PATCH}/boost_${BOOST_MAJOR}_${BOOST_MINOR}_${BOOST_PATCH}.tar.gz/download"
 mkdir /tmp/boost_src/
 tar -xzf /tmp/boost.tar.gz -C /tmp/boost_src/
 # This helps find then nested folder irrespective of verison.
@@ -18,6 +18,7 @@ cd $BOOST_ROOT
 ./b2 -j$(nproc) cxxflags="-fPIC" variant=release link=static --with-python --user-config="/python-config.jam" python="3.7" install
 ./b2 -j$(nproc) cxxflags="-fPIC" variant=release link=static --with-python --user-config="/python-config.jam" python="3.8" install
 ./b2 -j$(nproc) cxxflags="-fPIC" variant=release link=static --with-python --user-config="/python-config.jam" python="3.9" install
+./b2 -j$(nproc) cxxflags="-fPIC" variant=release link=static --with-python --user-config="/python-config.jam" python="3.10" install
 
 cd /
 rm -rf /tmp/boost_src/ /tmp/boost.tar.gz
